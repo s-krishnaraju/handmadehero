7836ead86d7223a3fb9dcff4b86dee10252e8596
Senthoor <skrishn3@terpmail.umd.edu>
2025-05-02 11:35:21 -0400

    some initial sdl stuff

--- src/sdl_handmade.cpp
+++ src/sdl_handmade.cpp
@@ -1,42 +1,46 @@
 #include <SDL.h>
+#include <stdlib.h>
 
-void DrawOnWindow(Uint32 WindowID){ 
-    SDL_Window *Window = SDL_GetWindowFromID(WindowID); 
-    SDL_Renderer *Renderer = SDL_GetRenderer(Window); 
-    static bool IsWhite = true; 
-    if(IsWhite){ 
-        SDL_SetRenderDrawColor(Renderer, 255,255,255,255); 
-        IsWhite = false; 
-    } else { 
-        SDL_SetRenderDrawColor(Renderer, 0, 0, 0, 255);
-        IsWhite = true;  
+#define internal static
+#define global_variable static
+
+global_variable bool Running;
+global_variable int TextureWidth;
+global_variable SDL_Texture* Texture;
+global_variable void* Pixels;
+const int PIXEL_SIZE = 4;
+
+internal void SDLResizeTexture(SDL_Renderer* Renderer, int Width, int Height) {
+    if (Texture) {
+        SDL_DestroyTexture(Texture);
     }
 
-    SDL_RenderClear(Renderer); 
-    SDL_RenderPresent(Renderer); 
+    if (Pixels) {
+        free(Pixels);
+    }
+
+    SDL_Texture* Texture = SDL_CreateTexture(Renderer, SDL_PIXELFORMAT_ABGR8888, SDL_TEXTUREACCESS_STREAMING, Width, Height);
+    int NumPixels = Width * Height;
+    void* Pixels = malloc(NumPixels * PIXEL_SIZE); //  Width*Height is num of pixels
+    TextureWidth = Width;
 }
 
-bool HandleEvent(SDL_Event *Event)
-{
-    bool ShouldQuit = false;
-
-    switch (Event->type)
-    {
+internal bool HandleEvent(SDL_Event* Event) {
+    switch (Event->type) {
     case SDL_WINDOWEVENT:
     {
-        SDL_WindowEvent window = Event->window;
-
-        switch (window.event)
-        {
+        SDL_WindowEvent WindowEvent = Event->window;
+        SDL_Window* window = SDL_GetWindowFromID(WindowEvent.windowID);
+        switch (WindowEvent.event) {
         case SDL_WINDOWEVENT_EXPOSED:
         {
-            DrawOnWindow(window.windowID); 
+
         }
         break;
 
         case SDL_WINDOWEVENT_RESIZED:
         {
-            printf("SDL_WINDOWEVENT_RESIZED (%d, %d)\n", window.data1, window.data2);
+            printf("SDL_WINDOWEVENT_RESIZED (%d, %d)\n", WindowEvent.data1, WindowEvent.data2);
         }
         break;
         }
@@ -46,7 +50,7 @@ bool HandleEvent(SDL_Event *Event)
     case SDL_QUIT:
     {
         printf("SDL QUIT\n");
-        ShouldQuit = true;
+        Running = false;
     }
     break;
 
@@ -55,37 +59,32 @@ bool HandleEvent(SDL_Event *Event)
     }
     break;
     }
-    return ShouldQuit;
 }
 
-int main(int argc, char *argv[])
-{
-
-    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+internal void StartEventLoop() {
+    while (Running) // Main event loop
     {
+        SDL_Event Event;
+        SDL_WaitEvent(&Event);
+        HandleEvent(&Event);
+    }
+}
+
+int main(int argc, char* argv[]) {
+
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
         // TODO: do something on error
     }
 
-    SDL_Window *Window;
-    Window = SDL_CreateWindow("Handmade Hero", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_RESIZABLE);
+    SDL_Window* Window = SDL_CreateWindow("Handmade Hero", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_RESIZABLE);
+    if (!Window)
+        return 0;
+    SDL_Renderer* Renderer = SDL_CreateRenderer(Window, -1, 0);
+    if (!Renderer)
+        return 0;
 
-    if (Window)
-    {
-        SDL_Renderer *Renderer = SDL_CreateRenderer(Window, -1, 0);
-        if (Renderer)
-        {
-            while (true) // Main event loop
-            {
-                SDL_Event Event;
-                SDL_WaitEvent(&Event);
-                if (HandleEvent(&Event))
-                {
-                    break; // Quit event loop
-                }
-            }
-        }
-    }
+    StartEventLoop();
 
     SDL_Quit();
-    return (0);
+    return 0;
 }
